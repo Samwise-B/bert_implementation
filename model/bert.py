5dbbf97e5df9a50c1b8cdabc6b23f13994fd63d2
Samwise-B <sam.burcher99@gmail.com>
2024-11-06 13:38:23 +0000

    Our single head transformer implementation

--- model/bert.py
+++ model/bert.py
@@ -48,6 +48,44 @@ class Naive(nn.Module):
         return self.relu(transformed)
 
 
+class OwnSingleHeadTransformer(nn.Module):
+    def __init__(self, embedding_dim: int, ff_dim: int):
+        super().__init__()
+        self.embed_dim = embedding_dim
+        self.scaling_fac = self.embed_dim ** (1 / 2)
+        self.M_q = nn.Linear(embedding_dim, embedding_dim)
+        self.M_k = nn.Linear(embedding_dim, embedding_dim)
+        self.M_v = nn.Linear(embedding_dim, embedding_dim)
+        self.ff = nn.Sequential(
+            nn.Linear(embedding_dim, ff_dim),
+            nn.ReLU(),
+            nn.Linear(ff_dim, embedding_dim),
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, embeddings: torch.Tensor):
+        # Embeddings: [batch_size, seq_len, embedding_dim]
+        Q = self.M_q(embeddings)
+
+        # [batch_size, seq_len, embedding_dim]
+        K = self.M_k(embeddings)
+
+        # [batch_size, seq_len, seq_len]
+        A_prime = torch.bmm(Q, K.transpose(-1, -2)) / self.scaling_fac
+
+        # [batch_size, seq_len, seq_len]
+        A = F.softmax(A_prime, dim=-1)
+
+        # [batch_size, seq_len, emb_dim]
+        V = self.M_v(embeddings)
+
+        # [batch_size, seq_len, embedding_dim]
+        attn_emb = torch.bmm(A, V)
+
+        # [batch_size, seq_len, embedding_dim]
+        return self.ff(attn_emb)
+
+
 if __name__ == "__main__":
     magic_layer = nn.Linear(768, 768)
     bert = BERP(magic_layer, 30522, 768)
